bccfd7df271a8e236dfa79aaadb3146e70642c6a
Pratijay Guha <pratijayguha1@gmail.com>
2025-01-25 23:46:21 -0500

    Added fields for data cleaning

---
+++ code/commons.py
@@ -0,0 +1,20 @@
+# <TODO> Add header doc.
+
+# Imports
+import os
+
+# PATH variables
+PROJECT_PATH = os.path.dirname(os.getcwd())
+DATA_PATH = os.path.join(PROJECT_PATH, "data")
+
+USERS_CSV_PATH = os.path.join(DATA_PATH, "raw", "USER_TAKEHOME.csv")
+TRANSACTIONS_CSV_PATH = os.path.join(DATA_PATH, "raw", "TRANSACTION_TAKEHOME.csv")
+PRODUCTS_CSV_PATH = os.path.join(DATA_PATH, "raw", "PRODUCTS_TAKEHOME.csv")
+
+USERS_SAMPLE_CSV_PATH = os.path.join(DATA_PATH, "raw", "sample", "users.csv")
+TRANSACTIONS_SAMPLE_CSV_PATH = os.path.join(
+    DATA_PATH, "raw", "sample", "transactions.csv"
+)
+PRODUCTS_SAMPLE_CSV_PATH = os.path.join(
+    DATA_PATH, "raw", "sample", "products.csv"
+)
