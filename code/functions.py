ee2bc9c3bca26a0d1d4358664a9f6715bc0f65c1
Pratijay Guha <pratijayguha1@gmail.com>
2025-01-27 10:51:00 -0500

    cleaned up `commons.py`

--- code/functions.py
+++ code/functions.py
@@ -2,9 +2,6 @@
 
 import io
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
 
 def standardize_datetime(df: pd.DataFrame, col_name: str, format: str):
     return pd.to_datetime(df[col_name], format=format, errors="coerce")
